36ae1c22fe935165a43a0c8d045e458b4bcddec6
rino.yamamoto <rino.matsushima@gmail.com>
2022-02-14 20:44:54 +0900

    pod installと.ignoreの作成まで実施

---
+++ taskapp/Task.swift
@@ -0,0 +1,27 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by 山本梨野 on 2022/02/14.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+
+    // タイトル
+    @objc dynamic var title = ""
+
+    // 内容
+    @objc dynamic var contents = ""
+
+    // 日時
+    @objc dynamic var date = Date()
+
+    // id をプライマリーキーとして設定
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -7,11 +7,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    }
+    
+    //各セルを選択したときに実行されるメソッド
+    //UITableViewDelegateプロトコルのメソッドで、セルをタップした時にタスク入力画面に遷移させる
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "cellSegue", sender: nil)
+    }
+    
+    //UITableViewDelegateプロトコルのメソッドで、セルが削除可能なことを伝えるメソッド
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return.delete
+    }
+    
+    //deleteボタンが押されたときに呼ばれるメソッド
+    //UITableViewDataSourceプロトコルのメソッドで、Deleteボタンが押されたときにローカル通知をキャンセルし、データベースからタスクを削除する
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        <#code#>
     }
 
 
